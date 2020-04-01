d34ac4d007f6e43dce4c0084d7510f4577fae478
Emerson Sousa <emersonuepb@gmail.com>
2020-03-31 23:45:20 -0300

    site app vacina campina primeiro commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import vue from 'vue';
+
+import navigation from 'components/navigation/navigation';
+import home from 'components/home/home';
+import divider from 'components/divider/divider';
+import feature from 'components/feature/feature';
+import download from 'components/download/download';
+import contact from 'components/contact/contact';
+import pageFooter from 'components/pageFooter/page-footer';
+
+import 'src/style.scss';
+
+new vue({
+  components: {
+    navigation,
+    home,
+    divider,
+    feature,
+    download,
+    contact,
+    pageFooter,
+  },
+}).$mount('#app');
